e4a0fcd379d0c1c1d5ee07d1bb84aeea09c519fb
Sead Niksic <seaddniksic@gmail.com>
2025-07-27 13:42:41 -0400

    updates to install script

--- install.sh
+++ install.sh
@@ -3,32 +3,55 @@ set -e
 
 echo "[*] Installing dependencies..."
 
-#install snap for alacritty
-sudo apt install snapd
+if ! command -v snap &> /dev/null; then
+    echo "installing snap..."
+    #install snap for alacritty
+    sudo apt install snapd
+else
+    echo "snap is already installed!"
+fi
 
-#install alacritty
-sudo snap install alacritty --classic
+if ! command -v alacritty &> /dev/null; then
+    echo "installing alacritty..."
+    #install alacritty
+    sudo snap install alacritty --classic
+else
+    echo "alacritty is already installed!"
+fi
 
-#install zsh
-sudo apt install zsh
+# if zsh isn't installed
+if ! command -v zsh &> /dev/null; then
+    echo "installing zsh..."
+    #install zsh
+    sudo apt install zsh
 
-#install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#install starship for prompt customization
-curl -sS https://starship.rs/install.sh | sh
+    #install oh my zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "zsh is already installed!"
+fi
 
 # install nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo rm nvim-linux-x86_64.tar.gz
+
+if ! command -v starship &> /dev/null; then
+    #install starship for prompt customization
+    curl -sS https://starship.rs/install.sh | sh
+else
+    echo "starship is already installed!"
+fi
 
 echo "[*] Configuring Themes..."
 
-mkdir -p alacritty/.config/alacritty/themes
+mkdir alacritty/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme alacritty/.config/alacritty/themes
 
 echo "[*] Stowing dotfiles..."
-stow alacritty nvim tmux zsh vim
+
+sudo apt install stow
+stow --target="$HOME" alacritty nvim tmux zsh vim
 
 echo "[*] Setup complete!"
